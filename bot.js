855711ba16da7ce119e41a5045703e66e5703358
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-19 13:50:07 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,25 @@
+client.on("ready", () => {
+let channel =     client.channels.get("500073403391410206")
+setInterval(function() {
+channel.send(`lol`);
+});
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+
+
